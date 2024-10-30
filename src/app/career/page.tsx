82c88d2e60426dd5cb8871eefa65affc7f180d0c
Kyle Sutton <kylesutton@the-creature.ht.home>
2024-10-30 00:05:35 -0400

    fix order of jobs in slideshow

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -20,74 +20,19 @@ type Job = {
     dates: string;
 }
 
-const jobsMobile: Job[] = [
-    {
-        image: O.some("https://s3.amazonaws.com/kylesutton-personal-website-photos/logos%2Fbl-logo.svg"),
-        name: "BondLink",
-        title: O.some("Functional TypeScript Engineer/Software Engineer II"),
-        dates: "2021 - 2024",
-        description: `
-            In 2021 I joined BondLink, a medium-sized startup that helps digitize the operations of municipal bond issuers and banks and helps connect issuers to
-            investors. As my first full-time job after graduating, BondLink gave me a chance to really come into my own as an engineer. Under the mentorship of some
-            truly skilled management and senior engineers, I developed better coding practices and really honed my skills in TypeScript and React. I also developed
-            skills in, and a deep appreciation for, functional programming with TypeScript using fp-ts, which was the standard at BondLink. In March 2023 I was promoted
-            to Software Engineer II, and took on more responsibilities, including mentorship of new team members and independent development on larger features,
-            as well as leadership roles on some collaborative projects. BondLink was a great place to work and gave me some of my most valuable experience to date.
-        `
-    },
-    {
-        image: O.some("https://s3.amazonaws.com/kylesutton-personal-website-photos/logos%2FairrLogo-inverted-transparent.png"),
-        name: "AIRR",
-        title: O.some("Fullstack Engineer"),
-        dates: "2019 - 2021",
-        description: `
-            At the end of my sophomore year I joined Airr, an early stage podcast listening startup, as a fullstack engineer. This was my most comprehensive experience with early startup culture,
-            working directly with the two cofounders in a tiny one-room office in Cambridge. Here I got more into the details of iOS app development with React
-            Native and developed more in-depth skills with MongoDB, Express, and Node.js. I worked with Airr either part-time or full-time for the rest of my college career, from summer 2019
-            until a few months after graduation in 2021, and took a semester off during fall 2020 to work full-time. During my work at Airr, I also did some independent pilot
-            research on building a recommendation algorithm, which gave me some real-world introduction to machine learning and big data work. Between joining the company and
-            my exit in 2021, I witnessed the app go from alpha to beta to full launch, ultimately hitting 50,000 users and 5,000 DAUs, and helped to deal with the issues
-            that arose with scaling to such a degree.
-        `
-    },
-    {
-        image: O.some("https://s3.amazonaws.com/kylesutton-personal-website-photos/logos%2Fdev.svg"),
-        name: "HSA DEV",
-        title: O.some("Fullstack Engineer"),
-        dates: "2018 - 2019",
-        description: `
-            While studying Computer Science at Harvard, I had the opportunity to work as a fullstack engineer for HSA DEV, a Harvard student-run development 
-            agency that took on various clients and helped make their app or website ideas a reality. I worked part-time during spring 2018 and during my sophomore
-            year in 2018-2019, and full time during the summer of 2018, getting a chance to cut my teeth in software engineering and to sample a
-            variety of different technologies and stacks, including both Express and GraphQL apis, SQL and noSQL databases, and mobile app development
-            with XCode and Android studio. It was also where I learned React and React Native, as well as my first exposure to Node.js for backend development.
-        `
-    },
-    {
-        image: O.none(),
-        name: "What's Next?",
-        title: O.none(),
-        dates: "2024 - ?",
-        description: `
-            I'm currently seeking full-time employment as a Software Engineer, and hoping to find a position where I can both continue to develop
-            the skills I already have and learn new ones, all while contributing meaningfully to an interesting product. I love to solve complex problems and
-            I thrive in a collaborative environment where I'm surrounded by talented coworkers. I'm excited to find what my next step will be!
-        `
-    },
-]
-
 const jobs: Job[] = [
     {
-        image: O.some("https://s3.amazonaws.com/kylesutton-personal-website-photos/logos%2Fdev.svg"),
-        name: "HSA DEV",
-        title: O.some("Fullstack Engineer"),
-        dates: "2018 - 2019",
+        image: O.some("https://s3.amazonaws.com/kylesutton-personal-website-photos/logos%2Fbl-logo.svg"),
+        name: "BondLink",
+        title: O.some("Functional TypeScript Engineer/Software Engineer II"),
+        dates: "2021 - 2024",
         description: `
-            While studying Computer Science at Harvard, I had the opportunity to work as a fullstack engineer for HSA DEV, a Harvard student-run development 
-            agency that took on various clients and helped make their app or website ideas a reality. I worked part-time during spring 2018 and during my sophomore
-            year in 2018-2019, and full time during the summer of 2018, getting a chance to cut my teeth in software engineering and to sample a
-            variety of different technologies and stacks, including both Express and GraphQL apis, SQL and noSQL databases, and mobile app development
-            with XCode and Android studio. It was also where I learned React and React Native, as well as my first exposure to Node.js for backend development.
+            In 2021 I joined BondLink, a medium-sized startup that helps digitize the operations of municipal bond issuers and banks and helps connect issuers to
+            investors. As my first full-time job after graduating, BondLink gave me a chance to really come into my own as an engineer. Under the mentorship of some
+            truly skilled management and senior engineers, I developed better coding practices and really honed my skills in TypeScript and React. I also developed
+            skills in, and a deep appreciation for, functional programming with TypeScript using fp-ts, which was the standard at BondLink. In March 2023 I was promoted
+            to Software Engineer II, and took on more responsibilities, including mentorship of new team members and independent development on larger features,
+            as well as leadership roles on some collaborative projects. BondLink was a great place to work and gave me some of my most valuable experience to date.
         `
     },
     {
@@ -106,17 +51,16 @@ const jobs: Job[] = [
         `
     },
     {
-        image: O.some("https://s3.amazonaws.com/kylesutton-personal-website-photos/logos%2Fbl-logo.svg"),
-        name: "BondLink",
-        title: O.some("Functional TypeScript Engineer/Software Engineer II"),
-        dates: "2021 - 2024",
+        image: O.some("https://s3.amazonaws.com/kylesutton-personal-website-photos/logos%2Fdev.svg"),
+        name: "HSA DEV",
+        title: O.some("Fullstack Engineer"),
+        dates: "2018 - 2019",
         description: `
-            In 2021 I joined BondLink, a medium-sized startup that helps digitize the operations of municipal bond issuers and banks and helps connect issuers to
-            investors. As my first full-time job after graduating, BondLink gave me a chance to really come into my own as an engineer. Under the mentorship of some
-            truly skilled management and senior engineers, I developed better coding practices and really honed my skills in TypeScript and React. I also developed
-            skills in, and a deep appreciation for, functional programming with TypeScript using fp-ts, which was the standard at BondLink. In March 2023 I was promoted
-            to Software Engineer II, and took on more responsibilities, including mentorship of new team members and independent development on larger features,
-            as well as leadership roles on some collaborative projects. BondLink was a great place to work and gave me some of my most valuable experience to date.
+            While studying Computer Science at Harvard, I had the opportunity to work as a fullstack engineer for HSA DEV, a Harvard student-run development 
+            agency that took on various clients and helped make their app or website ideas a reality. I worked part-time during spring 2018 and during my sophomore
+            year in 2018-2019, and full time during the summer of 2018, getting a chance to cut my teeth in software engineering and to sample a
+            variety of different technologies and stacks, including both Express and GraphQL apis, SQL and noSQL databases, and mobile app development
+            with XCode and Android studio. It was also where I learned React and React Native, as well as my first exposure to Node.js for backend development.
         `
     },
     {
@@ -185,8 +129,8 @@ const Job = (props: { job: Job }) => <div>
     </div>
 </div>
 
-const Timeline = (props: { jobs: Job[], initialSlide?: number }) => {
-    const [slide, setSlide] = useState({ slide: props.initialSlide ?? 0, direction: 1 });
+const Timeline = (props: { jobs: Job[] }) => {
+    const [slide, setSlide] = useState({ slide: 0, direction: 1 });
     const [transitions, api] = useTransition(slide.slide, () => ({
         from: { opacity: 0, x: slide.direction * 200 },
         enter: { opacity: 1, x: 0 },
@@ -255,8 +199,8 @@ const CareerPage = () => <div className="flex flex-col gap-y-4">
     <div className="mb-4">
         <PageHeader className="mb-4">My Career</PageHeader>
         <SubHeader className="mb-4">My Career Path</SubHeader>
-        <Timeline jobs={jobs} initialSlide={2} />
-        <TimelineMobile jobs={jobsMobile} />
+        <Timeline jobs={jobs} />
+        <TimelineMobile jobs={jobs} />
     </div>
     <div>
         <SubHeader className="mb-4">Short Term Projects and Contracts</SubHeader>
